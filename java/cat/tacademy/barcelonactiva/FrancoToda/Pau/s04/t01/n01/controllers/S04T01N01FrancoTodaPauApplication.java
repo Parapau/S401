10f4bf029fb9d39113dac7ba2dbc6cacd215cd7e
Parapau <120369026+Parapau@users.noreply.github.com>
2023-09-12 12:44:35 +0200

    Add files via upload

---
+++ java/cat/tacademy/barcelonactiva/FrancoToda/Pau/s04/t01/n01/controllers/S04T01N01FrancoTodaPauApplication.java
@@ -0,0 +1,14 @@
+package cat.tacademy.barcelonactiva.FrancoToda.Pau.s04.t01.n01.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01FrancoTodaPauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01FrancoTodaPauApplication.class, args);
+		
+	}
+
+}
